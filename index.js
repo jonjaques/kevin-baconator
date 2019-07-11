1e62badae95ddee92d70b7e17ac63023f76e8973
Jon Jaques <jon.jaques@alula.net>
2019-07-11 14:37:45 -0500

    Add some coloring/niceties

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const Inquirer = require("inquirer");
 const Wiki = require("wikijs").default;
 const Graph = require("graph-data-structure");
 const { union } = require("lodash");
+const Chalk = require("chalk");
+const { performance } = require("perf_hooks");
 
 const wiki = Wiki();
 const program = Yargs.argv;
 const query = program._.join(" ");
 
 const graph = Graph();
+let params;
 
 process.on("SIGTERM", () => process.exit(0));
 
@@ -17,28 +20,47 @@ main().catch(err => {
   return process.exit(1);
 });
 
+function getParams() {
+  return Inquirer.prompt([
+    { name: "source", message: `Who you lookin' fer?` },
+    { name: "target", message: `Kevin Bacon?`, default: "Kevin Bacon" }
+  ]);
+}
+
 async function main() {
-  let elapsed = console.time("total");
+  params = await getParams();
+  const now = performance.now();
+
   let gen = 0;
-  let nextGen = ["Kevin Bacon", query];
+  let nextGen = [params.source, params.target];
 
   let path = null;
 
   while (!path) {
     const currentGen = [...nextGen];
     nextGen = await processGeneration(gen, currentGen);
-    gen++;
     path = findPath();
+    gen++;
     await delay(1000);
   }
 
   if (path) {
-    let chords = path.slice(0);
+    let chords = path.slice(0).map(c => Chalk.bold(c));
     console.log(
-      `Found path: ${chords.join(" -> ")}, Bacon Factor: ${chords.length - 1}`
+      `${Chalk.green("Found path")}: ${chords.join(
+        Chalk.gray(" -> ")
+      )}, ${Chalk.cyan("Bacon Factor")}: ${Chalk.bold(chords.length - 1)}`
     );
   }
+
+  debugPerf("total", now);
   process.exit(0);
+}
+
+function debugPerf(name, ts) {
+  const now = performance.now();
+  const s = Chalk.bold.gray(((now - ts) / 1000).toFixed(2) + "s");
+  console.log(Chalk.gray(`${Chalk.bold.gray(name)}: completed in ${s}`));
 }
 
 function delay(ms) {
@@ -47,36 +69,48 @@ function delay(ms) {
 
 function findPath() {
   try {
-    return graph.shortestPath(query, "Kevin Bacon");
+    return graph.shortestPath(params.source, params.target);
   } catch (err) {
     return null;
   }
 }
 
 async function processGeneration(gen, nodes) {
-  console.time(`generation ${gen}`);
-  console.log(`starting generation ${gen}; size: ${nodes.length}`);
+  const now = performance.now();
+  console.log(
+    Chalk.gray(
+      `processing generation ${Chalk.yellow(gen)}; size: ${Chalk.yellow(
+        nodes.length
+      )}`
+    )
+  );
   const results = [];
   for (let node of nodes) {
     try {
-      console.log(`processing node ${node}`);
+      const nodeNow = performance.now();
+      console.log(Chalk.gray(`processing node ${Chalk.bold(node)}`));
       const links = await getPageLinks(node);
       if (!hasNode(node)) {
         graph.addNode(node);
       }
 
       links.forEach(edge => graph.addEdge(node, edge));
+      results.push(links);
+
       serializeGraph();
+
+      debugPerf(`node ${node}`, nodeNow);
       await delay(20);
+
       if (findPath()) {
         return results;
       }
-      results.push(links);
     } catch (err) {
-      console.log(`Error while fetching ${node}`, err);
+      console.log(Chalk.red(`Error while fetching ${node}`, err));
     }
   }
-  console.timeEnd(`generation ${gen}`);
+
+  debugPerf(`generation ${gen}`, now);
   return union(...results).sort();
 }
 
